2245318be5e34e32bce1d5dacd68ab599716ec1e
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-08-09 00:08:11 -0300

    feat: theme config

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,58 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  box: {
+    shadow: '0 8px 8px rgba(0, 0, 0, 0.08)'
+  },
+  font: {
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xxxxsmall: '0.8rem',
+      xxxsmall: '1.2rem',
+      xxsmall: '1.4rem',
+      xsmall: '1.6rem',
+      small: '2rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4rem',
+      xxlarge: '4.8rem',
+      xxxlarge: '5.6rem',
+      huge: '6.4rem'
+    }
+  },
+  colors: {
+    white: '#fff',
+    gray_100: '#c0c5ce',
+    gray_200: '#a7adba',
+    gray_300: '#65737e',
+    gray_400: '#4f5b66',
+    gray_500: '#343d46',
+    black: '#0D0D0D',
+    light_pink: '#ff71ce',
+    light_cian: '#01cdfe',
+    light_green: '#05ffa1',
+    light_purple: '#b967ff',
+    light_yellow: '#fffb96'
+  },
+  spacings: {
+    xxxsmall: '0.8rem',
+    xxsmall: '1rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+};
