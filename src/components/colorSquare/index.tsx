68ededf603aed47402bd055bbd4648b886ac973a
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-08-10 22:35:36 -0300

    feat: add 3 buttons easy, medium, hard

--- src/components/colorSquare/index.tsx
+++ src/components/colorSquare/index.tsx
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import * as S from './styles';
+import Image from 'next/image';
+import Skull from '@/assets/skull.svg';
+import Crown from '@/assets/crown-alt.svg';
+import Heart from '@/assets/heart-half-vertical.svg';
 
-const ColorSquare: React.FC<ColorSquareProps> = ({ color, progress }) => {
+const ColorSquare: React.FC<ColorSquareProps> = ({
+  color,
+  progress,
+  onEasyMode,
+  onMediumMode,
+  onHardMode
+}) => {
   const [progressWidth, setProgressWidth] = useState(0);
 
   useEffect(() => {
@@ -12,18 +22,20 @@ const ColorSquare: React.FC<ColorSquareProps> = ({ color, progress }) => {
     <div>
       <S.SquareContainer color={color}>
         <S.ProgressBar progress={progressWidth} />
-
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            background: 'red'
-          }}
-        >
-          <button>easy</button>
-          <button>midium</button>
-          <button>hard</button>
-        </div>
+        <S.ContainerStart>
+          <S.Button color="green" onClick={onEasyMode}>
+            <Image src={Heart} width={20} height={20} alt="" />
+            EASY
+          </S.Button>
+          <S.Button color="orange" onClick={onMediumMode}>
+            <Image src={Crown} width={20} height={20} alt="" />
+            MEDIUM
+          </S.Button>
+          <S.Button color="red" onClick={onHardMode}>
+            <Image src={Skull} width={20} height={20} alt="" />
+            HARD
+          </S.Button>
+        </S.ContainerStart>
       </S.SquareContainer>
     </div>
   );
--- src/components/colorSquare/styles.ts
+++ src/components/colorSquare/styles.ts
@@ -1,18 +1,40 @@
 import styled from 'styled-components';
 import theme from '@/styles/theme';
 
-export const SquareContainer = styled.div<SquareStyleProps>`
+export const SquareContainer = styled.div`
   width: 350px;
   height: 350px;
-  background-color: ${(props) => props.color};
   position: relative;
+  background-color: ${(props) => props.color};
 `;
 
 export const ProgressBar = styled.div<SquareStyleProps>`
-  width: ${(props) => props.progress}%;
-  height: 5px;
-  background-color: ${theme.colors.gray_100};
-  position: absolute;
   top: 0;
   left: 0;
+  height: 5px;
+  position: absolute;
+  width: ${(props) => props.progress}%;
+  background-color: ${theme.colors.gray_100};
+`;
+
+export const Button = styled.button<SquareStyleProps>`
+  gap: 2px;
+  color: #fff;
+  height: 50px;
+  width: 200px;
+  border-radius: 4px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: ${(props) => props.color};
+`;
+export const ContainerStart = styled.div`
+  gap: 10px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-inline: 20px;
+  justify-content: center;
 `;
