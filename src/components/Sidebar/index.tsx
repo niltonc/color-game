25c5f77423a9c5be1b30a028a707cbafbc9c6e17
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-08-11 10:10:14 -0300

    style: side bar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { SidebarContainer } from './styles';
+import {
+  ScoreContainer,
+  SidebarContainer,
+  Text,
+  TitleContainer
+} from './styles';
 
 const Sidebar: React.FC<ChildrenProp> = ({ children }) => {
-  return <SidebarContainer>{children}</SidebarContainer>;
+  return (
+    <SidebarContainer>
+      <div style={{}}>
+        <TitleContainer>Current / Lastest Game</TitleContainer>
+      </div>
+      <ScoreContainer>
+        <Text>Guessed Color</Text>
+        <Text>Correct Color</Text>
+        <Text>Score</Text>
+      </ScoreContainer>
+      {children}
+    </SidebarContainer>
+  );
 };
 
 export default Sidebar;
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -3,6 +3,24 @@ import theme from '@/styles/theme';
 
 export const SidebarContainer = styled.div`
   width: 250px;
+  color: ${theme.colors.primary};
+  box-shadow: ${theme.box.shadow};
   background-color: ${theme.colors.gray_50};
-  color: ${theme.colors.gray_400};
+`;
+export const ScoreContainer = styled.div`
+  padding: 5px;
+  display: flex;
+  margin-top: 10px;
+  align-items: center;
+  padding-inline: 20px;
+  border-top: 2px solid ${theme.colors.border};
+`;
+export const Text = styled.span`
+  font-size: 15px;
+`;
+export const TitleContainer = styled.div`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 1.19rem;
+  padding-inline: 20px;
 `;
